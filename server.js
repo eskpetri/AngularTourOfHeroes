dadbbdd708a9f027e64ae0da1858fcbec84fb583
Petri Eskelinen <eskpetri@gmail.com>
2022-11-13 16:30:26 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/heroes'));
+app.use(express.static(__dirname + '/dist/helloworld'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
-'/dist/heroes/index.html'));});
+'/dist/helloworld/index.html'));});
 app.listen(process.env.PORT || 8080);
