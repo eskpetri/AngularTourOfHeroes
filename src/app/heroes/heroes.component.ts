0ca4cb48aad72c1755738fb49110ae80fb6e9ada
Petri Eskelinen <eskpetri@gmail.com>
2022-11-13 09:10:35 +0200

    Add services

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+//import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -9,15 +11,21 @@ import { HEROES } from '../mock-heroes';
 })
 export class HeroesComponent implements OnInit {
 
-  heroes = HEROES;
+  heroes: Hero[] = []; //Hero Array to be injected
   selectedHero?: Hero;
 
-  constructor() { }
+  constructor(private heroService: HeroService, private messageService: MessageService) { }//Dependency injection 
 
+  getHeroes(): void {
+    this.heroService.getHeroes()                                                                        //Syncronous fetch: The HeroService.getHeroes() method has a synchronous signature (local mock array)
+        .subscribe(heroes => this.heroes = heroes);  //Serves when done let browser do what it wants and wakes up when ready(callback)
+  }             //On long fetch use loading screen                                                      //Beware you have to make asynchronous calls or that init is good. Pre Fetch?!?
   ngOnInit(): void {
+    this.getHeroes();
   }
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
 }
